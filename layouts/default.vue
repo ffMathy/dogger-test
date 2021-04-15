b47d897450b4cee0d75e2e16650e14becd51f880
kgnfth <73314940+kgnfth@users.noreply.github.com>
2021-04-15 21:42:05 +0200

    pull-doggy

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
 </template>
 
 <style>
-html {
+htmldasdasdasd {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
